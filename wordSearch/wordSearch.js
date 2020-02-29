6a71a0cdabfd7ed918f17680231d1ee096040619
Adam <adam.e.sheaffer@gmail.com>
2020-02-29 16:52:45 -0600

    add a solution

--- wordSearch/wordSearch.js
+++ wordSearch/wordSearch.js
@@ -1,4 +1,30 @@
-// Todo: Implement this function
-const wordExists = (board, word) => true;
+const wordExists = (board, word) => {
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[row].length; col++) {
+      const coords = [row, col];
+      // horizontal
+      if (searchFromLocation(coords, board, word, 1, 0)) return true;
+      // vertical
+      if (searchFromLocation(coords, board, word, 0, 1)) return true;
+      // diagonal up
+      if (searchFromLocation(coords, board, word, -1, 1)) return true;
+      // diagonal down
+      if (searchFromLocation(coords, board, word, 1, 1)) return true;
+    }
+  }
+  return false;
+};
+
+const searchFromLocation = (coords, board, word, xMovment, yMovement) => {
+  if (!word) return true;
+  const [row, col] = coords;
+  const nextCoords = [row + xMovment, col + yMovement];
+
+  return (
+    board[row] !== undefined &&
+    word[0] == board[row][col] &&
+    searchFromLocation(nextCoords, board, word.slice(1), xMovment, yMovement)
+  );
+};
 
 module.exports = wordExists;
